7a818d410c3818085248f5dda7fb7011aad686c4
Juan Fernandez Afonso <juferafo@hotmail.com>
2020-12-17 14:12:05 +0100

    dataframe-operations added

--- dataframe-operations/dataframe-operations.py
+++ dataframe-operations/dataframe-operations.py
@@ -8,6 +8,7 @@ data from repo -> https://github.com/databricks/LearningSparkV2.git
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 spark = SparkSession.builder\
     .getOrCreate()
@@ -49,17 +50,69 @@ fire_schema = StructType([
     ])
 
 data_df = spark.read.csv(data_path, schema=fire_schema, header=True)
-#data_df.show(10)
 
-# like spark.read spark.write can be use in the same way
-
-# Basic operations with dataframes in Spark
-
-# Saving Dataframe as parquet or SQL table
+# like spark.read spark.write can be use in the same way to save the Dataframe
+#   1. as a parquet file
+#   2. as an SQL table
 
 #data_df.write.format("parquet").save("/Users/mbjuan/Desktop/spark-playground/dataframe-operations/data.parquet")
+#data_df.write.format("parquet").saveAsTable("table")
 
-data_df.write.format("parquet").saveAsTable("table")
+# we can perform operations (transformations or actions) over the data
+
+new_fire_df = data_df\
+    .select("IncidentNumber", "AvailableDtTm", "CallType")\
+    .where(data_df.CallType == "Alarms")
+    
+#new_fire_df.show(truncate = False)
+    
+# How many distinct CallTypes were recorded as the causes of the fire calls?
+
+distinct_CallTypes = data_df\
+    .select("CallType")\
+    .where(col("CallType").isNotNull())\
+    .agg(countDistinct("CallType"))
+    
+distinct_CallTypes = data_df\
+    .select("CallType")\
+    .where(col("CallType").isNotNull())\
+    .distinct()
+    
+# Renaming columns
+
+rename_delay = data_df.withColumnRenamed("Delay", "ResponseDelayedinMins")\
+
+rename_delay = rename_delay\
+    .select("ResponseDelayedinMins")\
+    .filter(col("ResponseDelayedinMins") > 5)
+    
+# Convert fields into timestamp or date
+
+fire_ts_df = data_df\
+    .withColumn("IncidentDate", to_timestamp(col("CallDate"), "MM/dd/yyyy"))\
+    .drop("CallDate")\
+    .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "MM/dd/yyyy"))\
+    .drop("WatchDate")\
+    .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"),"MM/dd/yyyy hh:mm:ss a"))\
+    .drop("AvailableDtTm")\
+    .select("IncidentDate", "AvailableDtTS", "OnWatchDate")
+
+fire_ts_df.printSchema()
+
+#fire_ts_df.show(truncate = False)
+
+# how many calls were logged in the last seven days?
+
+last_week_df = fire_ts_df\
+    .withColumn("week", weekofyear(col("IncidentDate")))\
+    .withColumn("year", year(col("IncidentDate")))\
+    .groupBy("year", "week").agg(count("IncidentDate"))\
+    .orderBy("year", "week", ascending=False)\
+    .show()
+    
+
+
+
 
 spark.stop()
 
